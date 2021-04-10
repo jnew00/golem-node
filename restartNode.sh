a7c76fc1edd7f2c924ba4cc364c58ab0671eacf1
jnew00 <jnew00@gmail.com>
2021-04-09 22:03:51 -0400

    1

--- restartNode.sh
+++ restartNode.sh
@@ -2,31 +2,31 @@
 
 cd /home/jason/golem-node
 
-out=$(docker-compose exec $USE_TTY node golemsp status)
+out=$(docker-compose exec -T node golemsp status)
 #make status
-test -t 1 && USE_TTY="-t" 
+#test -t 1 && USE_TTY="-t" 
 #out=$(make status)
 # echo $out >> out.txt
 # cat out.txt
 # echo $out
 
 #
-# if echo $out | grep -q *'ERROR'*; then
-#    echo "Restarting..."
-#    make restart
-# else
-#     echo "No need to restart, displaying status..."
-#     # make status
-# fi
-
-#
-if [[ $out != *'No service registered under given address'* ]]; then
+if echo $out | grep -q *'ERROR'*; then
+   echo "Restarting..."
+   make restart
+else
     echo "No need to restart, displaying status..."
     # make status
-else
-    echo "Restarting..."
-    make restart
 fi
+
+#
+# if [[ $out != *'No service registered under given address'* ]]; then
+#     echo "No need to restart, displaying status..."
+#     # make status
+# else
+#     echo "Restarting..."
+#     make restart
+# fi
 
 # if echo $out | grep -q "ERROR"
 # then
